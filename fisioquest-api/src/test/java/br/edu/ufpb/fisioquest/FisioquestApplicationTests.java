ee1dbd805d10302479207f5dfbe52e07f38940e9
allanleandrof <alanleandrof@gmail.com>
2026-07-03 00:59:23 -0300

    feat: inicia spring

---
+++ fisioquest-api/src/test/java/br/edu/ufpb/fisioquest/FisioquestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufpb.fisioquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisioquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
