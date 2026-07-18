1b582d3823c804e574e8c345b2ea2c82228b9ee0
allanleandrof <alanleandrof@gmail.com>
2026-07-18 12:28:35 -0300

    feat: Implementar repositórios JPA

---
+++ fisioquest-api/src/main/java/br/edu/ufpb/fisioquest/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package br.edu.ufpb.fisioquest.repository;
+
+import br.edu.ufpb.fisioquest.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+}
