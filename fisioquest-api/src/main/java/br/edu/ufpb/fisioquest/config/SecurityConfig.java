0b4ddadda5bbd228e9f8151d32f7fdca05ab58e7
allanleandrof <alanleandrof@gmail.com>
2026-07-18 20:18:58 -0300

    feat: Implementar `AuthService` e `AuthController`

--- fisioquest-api/src/main/java/br/edu/ufpb/fisioquest/config/SecurityConfig.java
+++ fisioquest-api/src/main/java/br/edu/ufpb/fisioquest/config/SecurityConfig.java
@@ -9,7 +9,9 @@ import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -85,5 +87,10 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
